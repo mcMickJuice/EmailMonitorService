23ad06bfed9e991b7774555887bee804561cb2be
mcMickJuice <mikejoyce19@gmail.com>
2013-12-02 18:30:23 -0600

    Initial Commit

---
+++ EmailMonitorService/EmailClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Configuration;
+
+namespace EmailMonitorService
+{
+    class EmailClient
+    {
+        private SmtpClient _smtpClient;
+
+        public EmailClient()
+        {
+            string smtpHost = ConfigurationManager.AppSettings["SmtpServer"];
+            string senderAddress = ConfigurationManager.AppSettings["SenderAddress"];
+            string senderPassword = ConfigurationManager.AppSettings["SenderPassword"];
+
+            _smtpClient = new SmtpClient(smtpHost)
+            {
+                Credentials = new NetworkCredential(senderAddress, senderPassword),
+                EnableSsl = true,
+                Port = 465
+            };
+        }
+
+        public void SendMail(string from, string recipient, string subject, string body)
+        {
+            _smtpClient.Send(from, recipient,subject,body);
+            //_smtpClient.
+        }
+    }
+}
---
+++ EmailMonitorService/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailMonitorService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            //ServiceBase[] ServicesToRun;
+            //ServicesToRun = new ServiceBase[] 
+            //{ 
+            //    new Service1() 
+            //};
+            //ServiceBase.Run(ServicesToRun);
+
+            var mailClient = new EmailClient();
+
+            mailClient.SendMail("mjoyce@forthrightsolutions.com", "mikejoyce19@gmail.com","This is a test", "Testing testing testing");
+
+            Console.ReadKey();
+        }
+    }
+}
